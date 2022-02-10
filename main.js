b6695d6fc13cc1fb27d4bc5871c24738d9682cf8
the-groot <sangwoo0795@naver.com>
2022-02-10 16:32:23 +0900

    곡목록 불러오는거

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var url = require('url');
-var topic = require('./lib/topic');
 var author = require('./lib/author');
+const playlist=require('./lib/playlist.js');
 
 var app = http.createServer(function(request,response){
     var _url = request.url;
@@ -9,20 +9,20 @@ var app = http.createServer(function(request,response){
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        topic.home(request, response);
+        playlist.home(request, response);
       } else {
-        topic.page(request, response);
+        playlist.page(request, response);
       }
     } else if(pathname === '/create'){
-      topic.create(request, response);
+      playlist.create(request, response);
     } else if(pathname === '/create_process'){
-      topic.create_process(request, response);
+      playlist.create_process(request, response);
     } else if(pathname === '/update'){
-      topic.update(request, response);
+      playlist.update(request, response);
     } else if(pathname === '/update_process'){
-      topic.update_process(request, response);
+      playlist.update_process(request, response);
     } else if(pathname === '/delete_process'){
-      topic.delete_process(request, response);
+      playlist.delete_process(request, response);
     } else if(pathname === '/author'){
       author.home(request, response);
     } else if(pathname === '/author/create_process'){
