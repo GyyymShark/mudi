523d294637568f1e544417b1e26ddd10277e8434
the-groot <sangwoo0795@naver.com>
2022-02-15 22:59:12 +0900

    안쓰는 코드 삭제

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var url = require('url');
-var author = require('./lib/author');
 const playlist=require('./lib/playlist.js');
 const song=require('./lib/song.js');
 
@@ -37,16 +36,6 @@ var app = http.createServer(function(request,response){
       song.update(request, response);
     } else if(pathname=== '/song/update_process/'){
       song.update_process(request, response);
-    } else if(pathname === '/author'){
-      author.home(request, response);
-    } else if(pathname === '/author/create_process'){
-      author.create_process(request, response);
-    } else if(pathname === '/author/update'){
-      author.update(request, response);
-    } else if(pathname === '/author/update_process'){
-      author.update_process(request, response);
-    } else if(pathname === '/author/delete_process'){
-      author.delete_process(request, response);
     } else {
       response.writeHead(404);
       response.end('Not found');
